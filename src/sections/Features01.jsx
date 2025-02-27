e3f01aca179d20b9f8f5f4aa60497a50ef152fd9
gahyun <gahyun94526@gmail.com>
2025-02-27 17:59:22 +0900

    Feature 01 배경 애니메이션 무한 반복 구현 완료

--- src/sections/Features01.jsx
+++ src/sections/Features01.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { motion } from "framer-motion";
 
 const fadeInUp = {
@@ -11,64 +11,59 @@ const fadeInUp = {
   }),
 };
 
-const scrollingAnimation = {
-  animate: {
-    x: ["100%", "-100%"],
-    transition: {
-      repeat: Infinity,
-      duration: 10,
-      ease: "linear",
-    },
-  },
-};
+// keyframes 유틸리티로 애니메이션 정의
+const scrollIconsAnimation = keyframes`
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: -905px 0;
+  }
+`;
 
 const Features01 = () => {
   return (
     <Feature01Container>
-
-      <Icons
-        as={motion.img}
-        {...scrollingAnimation}
-        src={"/assets/images/f1_icon_all.png"}
-      />
-
-      <Content>
-        <Title
-          as={motion.h4}
-          custom={0}
-          variants={fadeInUp}
-          initial="hidden"
-          animate="visible"
-        >
-          Features 01
-        </Title>
-        <SubTitle
-          as={motion.h2}
-          custom={1}
-          variants={fadeInUp}
-          initial="hidden"
-          animate="visible"
-        >
-          웨이팅 신청 존에서 빠르게 웨이팅
-        </SubTitle>
-        <Description
-          as={motion.p}
-          custom={2}
-          variants={fadeInUp}
-          initial="hidden"
-          animate="visible"
-        >
-          장소에 도착하지 않고 빠르게 웨이팅을 신청할 수 있어요.
-        </Description>
-        <Phone
-          as={motion.img}
-          src={"/assets/images/f1_mo.svg"}
-          custom={3}
-          variants={fadeInUp}
-          initial="hidden"
-          animate="visible"
-        />
-      </Content>
+      <Feature01Content>
+        <Icons />
+        <Content>
+          <Title
+            as={motion.h4}
+            custom={0}
+            variants={fadeInUp}
+            initial="hidden"
+            animate="visible"
+          >
+            Features 01
+          </Title>
+          <SubTitle
+            as={motion.h2}
+            custom={1}
+            variants={fadeInUp}
+            initial="hidden"
+            animate="visible"
+          >
+            웨이팅 신청 존에서 빠르게 웨이팅
+          </SubTitle>
+          <Description
+            as={motion.p}
+            custom={2}
+            variants={fadeInUp}
+            initial="hidden"
+            animate="visible"
+          >
+            장소에 도착하지 않고 빠르게 웨이팅을 신청할 수 있어요.
+          </Description>
+          <Phone
+            as={motion.img}
+            src={"/assets/images/f1_mo.svg"}
+            custom={3}
+            variants={fadeInUp}
+            initial="hidden"
+            animate="visible"
+          />
+        </Content>
+      </Feature01Content>
     </Feature01Container>
   );
 };
@@ -81,6 +76,12 @@ const Feature01Container = styled.div`
   width: 100%;
   height: 670px;
   background: #171517;
+`;
+
+const Feature01Content = styled.div`
+  position: relative;
+  width: 800px;
+  margin: auto;
   overflow: hidden;
 `;
 
@@ -130,9 +131,12 @@ const Icons = styled.div`
   position: absolute;
   bottom: 7%;
   left: 50%;
+  transform: translateX(-50%);
   z-index: 1;
-  display: flex;
   width: 905px;
   height: 500px;
   overflow: hidden;
+  background: url("/assets/images/f1_icon_all.png") repeat-x;
+  background-size: auto 100%;
+  animation: ${scrollIconsAnimation} 10s linear infinite;
 `;
